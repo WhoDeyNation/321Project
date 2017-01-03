431548e5f6b689c62c6318bac4e9886570a5ae16
John Plate <vashachiroku@outlook.com>
2017-01-03 15:02:30 +0800

    Basic Outline
    Here is the basic outline as a starting point

---
+++ Platek_U2_NewAdUser.ps1
@@ -0,0 +1,21 @@
+# Bulk creating AD Users from a CSV file
+# Created by Aaron Platek aaron.platek11@outlook.com
+# Modules Required ActiveDirectory
+
+# Start of function 
+New-BulkADUser (OptionalParameters) 
+{
+
+# CSV File containing AD Users to be created
+$Csv = "<path>"
+
+# Foreach Loop to create AD Users in CSV file
+foreach ($User in $Users) 
+    {
+    
+    # AD Module cmdlet used to create new user from CSV
+    New-ADUser
+
+    }
+
+}
